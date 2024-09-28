5ff079939f4d9bcf3d610b54310eca066ea4a5d3
guru-harika <114151375+guru-harika@users.noreply.github.com>
2024-09-28 10:57:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MyMiniApp from "./components/MyMiniApp";
+import './App.css';
+// import MyMiniApp from './components/MyMiniApp';
+
+function App() {
+  return (
+    <>
+      <MyMiniApp />
+    </>
+  );
+}
+export default App;
